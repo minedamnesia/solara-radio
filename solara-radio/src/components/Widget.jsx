5475ba2fa38755ec87f6581a40ee09cc29ae4b1b
Kelly Simer <kelly.abdelmagid@rechargeapps.com>
2025-06-30 13:41:35 -0700

    Update Widget.jsx

--- solara-radio/src/components/Widget.jsx
+++ solara-radio/src/components/Widget.jsx
@@ -1,4 +1,5 @@
-// solara-radio-app/src/components/Widget.jsx
+import { Link } from 'react-router-dom';
+
 export default function Widget({ title, description, customClass, nestedWidget }) {
   return (
     <div className={`bg-coffee p-4 rounded-2xl shadow-lg ${customClass || ''}`}>
@@ -6,7 +7,11 @@ export default function Widget({ title, description, customClass, nestedWidget }
       <p className="font-sans mb-4 text-tan">{description}</p>
       {nestedWidget && (
         <div className="bg-sage p-4 rounded-xl shadow-md">
-          <h3 className="text-2xl font-heading mb-2 text-gunmetal">{nestedWidget.title}</h3>
+          <h3 className="text-2xl font-heading mb-2 text-gunmetal">
+            <Link to={nestedWidget.link} className="hover:underline">
+              {nestedWidget.title}
+            </Link>
+          </h3>
           <p className="font-sans text-gunmetal">{nestedWidget.description}</p>
         </div>
       )}
