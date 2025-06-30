5fe90bd97f864f39eb82a88e23adcf8fa0ebb395
Kelly Simer <kelly.abdelmagid@rechargeapps.com>
2025-06-30 13:15:54 -0700

    Update Widget.jsx

--- solara-radio/src/components/Widget.jsx
+++ solara-radio/src/components/Widget.jsx
@@ -1,12 +1,13 @@
+// solara-radio-app/src/components/Widget.jsx
 export default function Widget({ title, description, customClass, nestedWidget }) {
   return (
-    <div className={`bg-green-800 p-4 rounded-2xl shadow-lg ${customClass || ''}`}>
-      <h2 className="text-3xl font-heading tracking-wide mb-4">{title}</h2>
-      <p className="font-sans mb-4">{description}</p>
+    <div className={`bg-coffee p-4 rounded-2xl shadow-lg ${customClass || ''}`}>
+      <h2 className="text-3xl font-heading tracking-wide mb-4 text-persian-orange">{title}</h2>
+      <p className="font-sans mb-4 text-tan">{description}</p>
       {nestedWidget && (
-        <div className="bg-green-700 p-4 rounded-xl shadow-md">
-          <h3 className="text-2xl font-heading mb-2">{nestedWidget.title}</h3>
-          <p className="font-sans">{nestedWidget.description}</p>
+        <div className="bg-sage p-4 rounded-xl shadow-md">
+          <h3 className="text-2xl font-heading mb-2 text-gunmetal">{nestedWidget.title}</h3>
+          <p className="font-sans text-gunmetal">{nestedWidget.description}</p>
         </div>
       )}
     </div>
