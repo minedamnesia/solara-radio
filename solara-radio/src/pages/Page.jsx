beeac906676194902973e6e0b99572b4f64b3e92
Kelly Simer <kelly.abdelmagid@rechargeapps.com>
2025-06-30 12:43:01 -0700

    Update Page.jsx

--- solara-radio/src/pages/Page.jsx
+++ solara-radio/src/pages/Page.jsx
@@ -1,9 +1,8 @@
-// solara-radio-app/src/pages/Page.jsx
 export default function Page({ title }) {
   return (
     <div className="p-4 bg-green-800 rounded-2xl shadow-lg">
-      <h2 className="text-3xl font-semibold mb-4">{title}</h2>
-      <p>This is the {title} page. Content coming soon!</p>
+      <h2 className="text-4xl font-heading tracking-wide mb-4">{title}</h2>
+      <p className="font-sans">This is the {title} page. Content coming soon!</p>
     </div>
   );
 }
