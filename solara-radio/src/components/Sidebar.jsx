cb367d74b43fe15765841dcfa2fea0fc2d18737f
Kelly Simer <kelly.abdelmagid@rechargeapps.com>
2025-07-10 15:37:09 -0700

    add sidebar style

--- solara-radio/src/components/Sidebar.jsx
+++ solara-radio/src/components/Sidebar.jsx
@@ -4,10 +4,16 @@ import BuyMeCoffeeWidget from '../components/BuyMeCoffeeWidget';
 
 export default function SidebarRight() {
   return (
-    <aside className="w-full md:w-64 p-4">
-      <CompassWidget />
-      <MaidenheadWidget />
-      <BuyMeCoffeeWidget />
+    <aside className="w-full md:w-64 p-4 text-coffee">
+      <div className="sidebar-widget">
+        <CompassWidget />
+      </div>
+      <div className="sidebar-widget">
+        <MaidenheadWidget />
+      </div>
+      <div className="sidebar-widget">
+        <BuyMeCoffeeWidget />
+      </div>
     </aside>
   );
 }
