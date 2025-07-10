af0be8015e191b39be268deeef5cc754faa7f3ae
Kelly Simer <kelly.abdelmagid@rechargeapps.com>
2025-07-10 11:56:25 -0700

    fix double rendering problem

--- solara-radio/src/App.js
+++ solara-radio/src/App.js
@@ -1,4 +1,3 @@
-// solara-radio-app/src/App.jsx
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Home from './pages/Home';
@@ -9,8 +8,14 @@ export default function App() {
     <div className="bg-gunmetal text-persian-orange min-h-screen flex">
       <div className="flex-1 p-6">
         <Header />
-        <Home />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/page" element={<Page />} />
+          {/* Add more routes here as needed */}
+        </Routes>
       </div>
     </div>
   );
 }
+
