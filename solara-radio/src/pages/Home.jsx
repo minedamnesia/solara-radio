f6764b77a29242d091e5e8ba02397bd0804bf275
Kelly Simer <kelly.abdelmagid@rechargeapps.com>
2025-07-10 23:23:14 -0700

    but will it stack?

--- solara-radio/src/pages/Home.jsx
+++ solara-radio/src/pages/Home.jsx
@@ -14,14 +14,18 @@ export default function Home() {
       {/* Main Grid */}
     <main className="w-[80%] p-6 grid grid-cols-3 grid-rows-4 gap-6">
       <HamRadioResourcesWidget />
-      <Widget
-        title="About Me"
-        description="Hi! I'm Kelly, otherwise known as KK7QEA..."
-        customClass="row-span-2"
-        link="/about"
-        image="/qsl.png"
-      />
-      <SpotifySCMEmbedWidget />
+      <div className="col-span-1 row-span-1">
+        <Widget
+          title="About Me"
+          description="Hi! I'm Kelly, otherwise known as KK7QEA..."
+          customClass="row-span-2"
+          link="/about"
+          image="/qsl.png"
+        />
+      </div>
+      <div className="col-span-1 row-span-1">
+        <SpotifySCMEmbedWidget />
+      </div>
       <LocalPlantsWidget />
       <HikingMapsWidget />
       <SolarPositionsWidget />
